70be018b99410d3811b54d115baa5c69d61df566
Carlos Wilson <carlos.wilsong@sempreceub.com>
2025-05-09 18:52:09 -0300

    Add files via upload

---
+++ Atividade C, questão 1,1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+float = num1 , num2, num3,media;
+
+printf("Escreva tres numeros: \n");
+scanf("%f %f %f,"&num1, &num2, &num3);
+
+média = (num1+ num2+ num3)/3;
+printf("O resultado da media é :%.2f", media);
+
+}
